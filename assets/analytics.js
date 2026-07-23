e4dd659d609f0a15612b9a9f2692bfa0e122a38f
joao2659 <73093115+joao2659@users.noreply.github.com>
2026-07-22 22:48:26 -0300

    Create analytics.js

---
+++ assets/analytics.js
@@ -0,0 +1,12 @@
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-K5098N4VN8';
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', 'G-K5098N4VN8');
+})();
